07af2ff45a22c4b2e4c540842a900f29a72cb6a1
Hiago Lucas <hcardealdemelo@gmail.com>
2026-06-20 14:01:25 -0300

    tuples and comprehension lists

--- 001-starting-out/baby.hs
+++ 001-starting-out/baby.hs
@@ -1,4 +1,9 @@
 doubleMe x = x + x
+
 doubleUs x y = doubleMe x + doubleMe y
-doubleSmallNumber x = if x <= 100 then x*2 else x
-doubleSmallNumber' x = (if x <= 100 then x*2 else x) + 1
+
+doubleSmallNumber x = if x <= 100 then x * 2 else x
+
+doubleSmallNumber' x = (if x <= 100 then x * 2 else x) + 1
+
+rightTriangles = [(y,z,x)| x <- [1..10], y <- [1..x], z <- [1..y], y ^ 2 + z ^ 2 == x ^ 2, x + y + z == 24]
